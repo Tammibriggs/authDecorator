304d74b784fc9f37cb77b2496d6df33275bf6fd8
Tammibriggs <Godsfirstbriggs@gmail.com>
2021-07-13 00:27:21 +0100

    created registeration page

--- authApp/views.py
+++ authApp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == 'POST':
+        register_form = UserCreationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            return redirect('login')
+    else:
+        register_form = UserCreationForm()
+    return render(request, 'register.html', {'register_form': register_form})
